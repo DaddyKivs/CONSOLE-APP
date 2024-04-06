30afb72fb3ccda9843ab30b63d0ce89459bedc70
DaddyKivs <159437105+DaddyKivs@users.noreply.github.com>
2024-04-06 10:26:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello World, This is Percival Omari Greeting You From The Other Side Of the screen!");
+Console.WriteLine("The Current Time is " + DateTime.Now);
+Console.WriteLine("Signing out now." );
+Console.WriteLine("Have a great week ahead ;)");
+Console.WriteLine("Bye");
